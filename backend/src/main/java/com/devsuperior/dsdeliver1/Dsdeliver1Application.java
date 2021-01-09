f6b0a05b0c6da84dba81f48fd27f21790bec612d
Claudia <claudiadecarvalho@gmail.com>
2021-01-09 16:45:38 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/dsdeliver1/Dsdeliver1Application.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsdeliver1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dsdeliver1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dsdeliver1Application.class, args);
+	}
+
+}
